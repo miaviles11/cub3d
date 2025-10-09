fffd0d48e304b5920fbda986a0e9ef60383df1bd
CarlosVSL <carlosvsanchezlopez@gmail.com>
2025-10-09 17:37:54 +0200

    Update init_mlx.c

--- src_bonus/init/init_mlx.c
+++ src_bonus/init/init_mlx.c
@@ -12,58 +12,50 @@
 
 #include "../../include_bonus/cub3d.h"
 
-static void	setup_mouse(t_cub *cub)
-{
-	mlx_mouse_hide(cub->mlx, cub->win);
-	mlx_mouse_move(cub->mlx, cub->win, WIN_W / 2, WIN_H / 2);
-	cub->mouse_x = WIN_W / 2;
-	cub->first_mouse = 1;
-}
-
 static int	init_zbuffer(t_cub *cub)
 {
 	int	i;
 
-	cub->z_buffer = malloc(sizeof(double) * WIN_W);
+	cub->z_buffer = (double *)malloc(sizeof(double) * WIN_W);
 	if (!cub->z_buffer)
 		return (-1);
 	i = 0;
 	while (i < WIN_W)
 	{
 		cub->z_buffer[i] = 0.0;
-		i++;
+		++i;
 	}
 	return (0);
 }
 
-static int	init_screen_image(t_cub *cub)
+void	setup_mouse_deferred(t_cub *cub)
 {
-	cub->screen.ptr = mlx_new_image(cub->mlx, WIN_W, WIN_H);
-	if (!cub->screen.ptr)
-		return (-1);
-	cub->screen.data = (int *)mlx_get_data_addr(cub->screen.ptr,
-			&cub->screen.bpp,
-			&cub->screen.line_len,
-			&cub->screen.endian);
-	if (!cub->screen.data)
-		return (-1);
-	cub->screen.w = WIN_W;
-	cub->screen.h = WIN_H;
-	return (0);
+	if (!cub || !cub->mlx || !cub->win)
+		return ;
+	mlx_mouse_hide(cub->mlx, cub->win);
+	mlx_mouse_move(cub->mlx, cub->win, WIN_W / 2, WIN_H / 2);
+	cub->mouse_x = WIN_W / 2;
+	cub->first_mouse = 1;
 }
 
 int	init_mlx(t_cub *cub)
 {
+	if (!cub)
+		return (-1);
 	cub->mlx = mlx_init();
 	if (!cub->mlx)
 		return (-1);
 	cub->win = mlx_new_window(cub->mlx, WIN_W, WIN_H, "cub3D");
 	if (!cub->win)
 		return (-1);
-	if (init_screen_image(cub) == -1)
-		return (-1);
 	if (init_zbuffer(cub) == -1)
 		return (-1);
-	setup_mouse(cub);
+	cub->screen.ptr = NULL;
+	cub->screen.data = NULL;
+	cub->screen.w = 0;
+	cub->screen.h = 0;
+	cub->screen.bpp = 0;
+	cub->screen.line_len = 0;
+	cub->screen.endian = 0;
 	return (0);
 }
